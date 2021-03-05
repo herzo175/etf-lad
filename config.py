7155cced9e8ff7bd4abff96d61f0b57c8386ebc9
Jeremy Herzog <jeremyaherzog@gmail.com>
2021-03-04 21:54:31 -0600

    add retry logic for trades

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ import dotenv
 
 dotenv.load_dotenv()
 
-POOL_SIZE=int(os.getenv("POOL_SIZE", "5"))
-LOOKBACK_PERIOD=int(os.getenv("LOOKBACK_PERIOD", "7"))
-IEX_KEY=os.getenv("IEX_KEY")
-APCA_API_KEY_ID=os.getenv("APCA_API_KEY_ID")
-APCA_API_KEY_SECRET=os.getenv("APCA_API_KEY_SECRET")
-APCA_ENDPOINT=os.getenv("APCA_ENDPOINT", "https://paper-api.alpaca.markets")
+POOL_SIZE = int(os.getenv("POOL_SIZE", "5"))
+LOOKBACK_PERIOD = int(os.getenv("LOOKBACK_PERIOD", "7"))
+IEX_KEY = os.getenv("IEX_KEY")
+APCA_API_KEY_ID = os.getenv("APCA_API_KEY_ID")
+APCA_API_KEY_SECRET = os.getenv("APCA_API_KEY_SECRET")
+APCA_ENDPOINT = os.getenv("APCA_ENDPOINT", "https://paper-api.alpaca.markets")
+PICKLE_FILE_NAME = os.getenv("PICKLE_FILE_NAME", "pool.pkl")
